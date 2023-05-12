2f02fa909beba92c092171f16ca3dba711c40578
Snehashish Laskar <snehashish.laskar1@gmail.com>
2023-05-13 01:27:44 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 cd /usr/local/bin
 mkdir menousdb-source-code
 cd menousdb-source-code
-curl -o api.py https://raw.githubusercontent.com/snehashish090/MenousDB-Official/stable/src/api.py
-curl -o auth.py https://raw.githubusercontent.com/snehashish090/MenousDB-Official/stable/src/auth.py
-curl -o models.py https://raw.githubusercontent.com/snehashish090/MenousDB-Official/stable/src/models.py
-curl -o requirements.txt https://raw.githubusercontent.com/snehashish090/MenousDB-Official/stable/requirements.txt
+curl -o api.py https://snehashish090.github.io/src/api.py
+curl -o auth.py https://snehashish090.github.io/src/auth.py
+curl -o models.py https://snehashish090.github.io/src/models.py
+
+curl -o requirements.txt https://snehashish090.github.io/requirements.txt
 pip3 install -r requirements.txt
 pip3 install pyinstaller
-python3 -m pyinstaller api.py --onefile
+echo "export PATH=$PATH:/Library/Frameworks/Python.framework/Versions/3.11/bin/" >> ~/.zshrc
+echo "export PATH=$PATH:/Library/Frameworks/Python.framework/Versions/3.11/bin/" >> ~/.bashrc
+pyinstaller api.py --onefile
 mv dist/api /usr/local/bin/menousdb
 cd /usr/local/bin
 rm -r menousdb-source-code
